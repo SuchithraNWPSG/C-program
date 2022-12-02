5c359aa9426b4666154ebe4ee5eb8438da035898
SuchithraNWPSG <118951268+SuchithraNWPSG@users.noreply.github.com>
2022-12-02 10:46:18 +0530

    Add files via upload

---
+++ calculator(switch).c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+  
+  char cal;
+  int n1,n2;
+
+  printf("Enter First Number \n");
+  scanf("%d",&n1);
+  printf("Enter Second Number \n");
+  scanf("%d",&n2);
+
+  printf("Enter Cal \n");
+  scanf(" %c",&cal);
+  
+  switch(cal)
+  {
+    case '+':
+    {printf("Answer is %d",n1+n2);
+    break;}
+
+    case '-':
+    {printf("Answer is %d",n1-n2);
+    break;}
+
+    case '*':
+    {printf("Answer is %d",n1*n2);
+    break;}
+
+    case '/':
+    {printf("Answer is %d",n1/n2);
+    break;}
+  }
+}
